6bdd3272a0247e9aa6fc72ed20fda6b69e722be3
Orkhan <aliyevorhan82@gmail.com>
2024-11-16 01:45:57 +0400

    добавил конфигурацию и провайдеры для настройки

---
+++ MongoBackupHelperApp/Config/AppConfig.cs
@@ -0,0 +1,8 @@
+﻿namespace MongoBackupHelperApp.Config
+{
+    internal class AppConfig
+    {
+        public string ConnectionString { get; set; } = string.Empty;
+        public string BackupFolder {  get; set; } = string.Empty;
+    }
+}
--- MongoBackupHelperApp/Program.cs
+++ MongoBackupHelperApp/Program.cs
@@ -1,4 +1,10 @@
-﻿using MongoDB.Driver;
+﻿
+
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using MongoBackupHelperApp.Config;
+using System.Security.Authentication.ExtendedProtection;
 
 namespace MongoBackupHelperApp
 {
@@ -6,12 +12,38 @@ namespace MongoBackupHelperApp
     {
         static async Task Main(string[] args)
         {
-            AppConfig();
+            try
+            {
+                AppConfig();
+
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+           
         }
 
         private static void AppConfig()
         {
-            throw new NotImplementedException();
+
+            var serviceCollection = new ServiceCollection();
+
+            var builder= new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                // optional false — файл обязателен, и приложение завершится с ошибкой, если файл не найден.
+                // reloadonchange true — конфигурация будет автоматически обновляться, если файл изменится.
+                .AddJsonFile("options1.json",optional:false,reloadOnChange:true) 
+                .Build();
+
+            serviceCollection.Configure<AppConfig>(builder.GetRequiredSection("Options"));
+
+            var serviceProvider= serviceCollection.BuildServiceProvider();
+
+            // Пример получения конфигурации из DI контейнера через IOptions
+            var appConfig = serviceProvider.GetRequiredService<IOptions<AppConfig>>().Value;
+
+            Console.WriteLine($"Connection String: {appConfig.ConnectionString}");
+            Console.WriteLine($"Backup Folder: {appConfig.BackupFolder}");
         }
     }
 }
