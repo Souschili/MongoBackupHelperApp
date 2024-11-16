96ce3489ed3bd8990024aaaa5cea15ed62e65536
Orkhan <aliyevorhan82@gmail.com>
2024-11-16 18:58:32 +0400

    добавил получение очищеных имен файлов как список

--- MongoBackupHelperApp/Services/FileManagerService.cs
+++ MongoBackupHelperApp/Services/FileManagerService.cs
@@ -1,16 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Options;
 
 namespace MongoBackupHelperApp.Services
 {
     internal class FileManagerService
     {
-        public void ParseFile()
+        private readonly IOptions<AppConfig> _appConfig;
+
+        public FileManagerService(IOptions<AppConfig> appConfig)
         {
-            Console.WriteLine("Here we start parse");
+            appConfig.Value.Validate();
+            _appConfig = appConfig;
+        }
+
+        public IEnumerable<string> GetFiles()
+        {
+            // чекаем есть ли эта директория физически
+            if(!Directory.Exists(_appConfig.Value.BackupFolder))
+            {
+                throw new InvalidOperationException($"Unable to find backup directory path {_appConfig.Value.BackupFolder}");
+            }
+
+            // получаем список файлов,убираем полный путь ,если их нет пишем что ноль и выходим
+            var files=new DirectoryInfo(_appConfig.Value.BackupFolder)
+                .EnumerateFiles()
+                .Select(x=> x.Name);
+
+            if (!files.Any())
+                throw new Exception($"Files to upload not found in {_appConfig.Value.BackupFolder}");
+            
+           return files;
         }
     }
 }
